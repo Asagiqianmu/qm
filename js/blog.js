7e800e9791cadaef1b80ff57199aeab51a51d369
zhougan <1175609090@qq.com>
2018-06-07 19:39:32 +0800

    Repair Bug

--- js/blog.js
+++ js/blog.js
@@ -1,6 +1,11 @@
 
 $(document).ready(function() {
     var  isopen = true;
+    var all= $(".blog_conter_all");
+    var css= $(".blog_conter_css");
+    var html= $(".blog_conter_html");
+    var js= $(".blog_conter_js");
+    /*var other= $(".blog_conter_other);*/
         $('#menu_list').click(function () {
 
                 $("#blog_left").toggleClass("blog_left_on");
@@ -11,4 +16,24 @@ $(document).ready(function() {
 
 
         })
+    $("#all").click(function () {
+       all.css("display","block");
+       all.css("display","block");
+    });
+
+    $("#css").click(function () {
+        css.css("display","block");
+    });
+
+    $("#html").click(function () {
+        html.css("display","block");
+    });
+
+    $("#js").click(function () {
+        js.css("display","block");
+    });
+
+    $("#other").click(function () {
+        other.css("display","block");
+    });
 })
