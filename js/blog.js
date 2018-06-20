1f97bd8362d15e6bc0f7e940718855de732b8442
Asagiqiamu <1175609090@qq.com>
2018-06-20 14:43:55 +0800

    Repair Bug

--- js/blog.js
+++ js/blog.js
@@ -6,44 +6,52 @@ $(document).ready(function() {
     var html= $(".blog_conter_html");
     var js= $(".blog_conter_js");
     var other = $(".blog_conter_other");
+
     function menuon_off(){
         $("#blog_left").toggleClass("blog_left_on");
         $("#blog_list").toggleClass("blog_list_on");
         $("#blog_list").toggleClass("blog_list");
         $("#blog_left").toggleClass("blog_left");
-        $(".text_head").css("z-index", "20");
-    }
 
+    }
+    function menuon_close(){
+        $("#blog_left").removeClass("blog_left_on");
+        $("#blog_list").removeClass("blog_list_on");
+        $("#blog_list").addClass("blog_list");
+        $("#blog_left").addClass("blog_left");
+
+    }
     $('#menu_list').click(function () {
         menuon_off();
     });
     $("#all").click(function () {
        all.css("display","block");
        $(".blog_conter_css,.blog_conter_html,.blog_conter_js,.blog_conter_other").css("display","none");
-        menuon_off();
+        menuon_close();
     });
 
     $("#css").click(function () {
         css.css("display","block");
         $(".blog_conter_all,.blog_conter_html,.blog_conter_js,.blog_conter_other").css("display","none");
-        menuon_off();
+        menuon_close();
+
     });
 
     $("#html").click(function () {
         html.css("display","block");
         $(".blog_conter_all,.blog_conter_css,.blog_conter_js,.blog_conter_other").css("display","none");
-        menuon_off();
+        menuon_close();
     });
 
     $("#js").click(function () {
         js.css("display","block");
         $(".blog_conter_all,.blog_conter_html,.blog_conter_css,.blog_conter_other").css("display","none");
-        menuon_off();
+        menuon_close();
     });
     $("#other").click(function () {
         other.css("display","block");
         $(".blog_conter_all,.blog_conter_html,.blog_conter_css,.blog_conter_js").css("display","none");
-        menuon_off();
+        menuon_close();
     });
 
 })
